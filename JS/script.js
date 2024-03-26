e46dce081f972071e7635cd7927d6c35d812945c
Alexandr <alexandryuriev98@gmail.com>
2024-03-26 23:24:38 +0700

    for if else practice

--- JS/script.js
+++ JS/script.js
@@ -21,6 +21,22 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит -
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше -
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// Код возьмите из предыдущего домашнего задания
+
 'use strict';
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
@@ -33,23 +49,40 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done')
+    } else {
+        console.log('Error');
+        i--;
+    }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (30 >= personalMovieDB.count >= 10) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка')
+};
 
 console.log(personalMovieDB);
 
 
 
-if (4 == 0) {
-    console.log('ok!');
-} else {
-    console.log('Not Ok');
-};
+
+
+// if (4 == 0) {
+//     console.log('ok!');
+// } else {
+//     console.log('Not Ok');
+// };
 
 
 
@@ -64,25 +97,25 @@ if (4 == 0) {
 
 // (num === 50) ? console.log('ok') : console.log('Error');
 
-const num = 50;
+// let num = 50;
 
-switch (num) {
-    case 49:
-        console.log('Error');
-        break;
-    case 100:
-        console.log('Error');
-        break;
-    case 50:
-        console.log('ok');
-        break;
-    default:
-        console.log('Not this time');
-        break;
-};
+// switch (num) {
+//     case 49:
+//         console.log('Error');
+//         break;
+//     case 100:
+//         console.log('Error');
+//         break;
+//     case 50:
+//         console.log('ok');
+//         break;
+//     default:
+//         console.log('Not this time');
+//         break;
+// };
 
 
-let num = 50;
+// let num = 50;
 
 // while (num <= 55) {
 //     console.log(num);
@@ -98,10 +131,14 @@ let num = 50;
 // while (num < 55);
 
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    console.log(i);
-}
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// };
+
+
+
+
