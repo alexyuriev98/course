2bca32511e95beedc8343ee98107525ab7c07894
Alexandr <alexandryuriev98@gmail.com>
2024-03-26 22:31:17 +0700

    for

--- JS/script.js
+++ JS/script.js
@@ -80,3 +80,28 @@ switch (num) {
         console.log('Not this time');
         break;
 };
+
+
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// };
+
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+
+// while (num < 55);
+
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+}
